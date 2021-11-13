dedd047cd366c2344175aab86a15b8122429c0a1
Aaron Janes <aaron.janes@gmail.com>
2021-11-12 21:51:18 -0700

    added colors working game

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use term_painter::Color::*;
 
+use term_painter::ToStyle;
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    println!("{}", Red.paint("Guess the secret number!"));
+    println!("{}",  Red.paint("Please input your guess."));
     
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("The secret number is: {}", secret_number);
+   
 
     loop {
         let mut guess = String::new();
@@ -15,13 +17,19 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
     
-        let guess:u32 = guess.trim().parse().expect("Please type a number");
-        println!("You guessed: {}", guess);
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("{}{}", Green.paint("You guessed:"), Green.paint(guess));
     
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal =>println!("You win!"),
+            Ordering::Less => println!("{}", Magenta.paint("Too small!")),
+            Ordering::Greater => println!("{}" , BrightYellow.paint("Too big!")),
+            Ordering::Equal =>{
+                println!("{} {}", BrightCyan.paint("You win! the secret number was:"), secret_number);
+                break;
+            }
         }
 
 
